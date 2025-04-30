60a64b4144b798d95519baa2c4af5810066df48c
Maybemarc <marcspectre777@gmail.com>
2025-04-30 22:08:44 +0530

    structure build

---
+++ Frontend/e-commerce/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <div>From Navbar</div>;
+}
+
+export default Navbar;
---
+++ Frontend/e-commerce/src/pages/ProductDetail.jsx
@@ -0,0 +1,9 @@
+function ProductDetail() {
+  return (
+    <div>
+      <h1>SPecificProduct</h1>
+    </div>
+  );
+}
+
+export default ProductDetail;
