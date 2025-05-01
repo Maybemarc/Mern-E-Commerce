730877a637f0297120b036c5865e17e2c5079b8c
Maybemarc <marcspectre777@gmail.com>
2025-05-01 22:57:46 +0530

    created new page

--- Frontend/e-commerce/src/App.jsx
+++ Frontend/e-commerce/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./components/SignUp";
 import ProductDetail from "./pages/ProductDetail";
 import "./App.css";
 import Cart from "./pages/cart";
+import Orders from "./pages/orders";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="ProductDetail/:id" element={<ProductDetail />} />
-        <Route path="user/cart" element={<Cart />}  />
+        <Route path="user/cart" element={<Cart />} />
+        <Route path="user/order" element={<Orders />} />
       </Routes>
     </div>
   );
---
+++ Frontend/e-commerce/src/pages/orders.jsx
@@ -0,0 +1,9 @@
+function Orders(){
+    return (
+        <div>
+            <h1>orders Page</h1>
+        </div>
+    )
+}
+
+export default Orders
