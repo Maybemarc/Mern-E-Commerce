d778e0c10d72b61d86db77f2cbd6b600e900917a
Maybemarc <marcspectre777@gmail.com>
2025-05-20 12:45:15 +0530

    Categories added

--- Frontend/e-commerce/src/components/Categories.jsx
+++ Frontend/e-commerce/src/components/Categories.jsx
@@ -6,25 +6,80 @@ function Categories() {
           <h2>Categories</h2>
         </div>
         <div className="category_Types">
-          <div className="Category_List">
-            <p>T-Shirt</p>
-            <span> &#8594;</span>
+          <div className="Category_List Fill_Color1">
+            <img
+              src="./src/assets/icons/shoe-svgrepo-com.png"
+              width="50px"
+              height="50px"
+            />
+            <p>Shoes</p>
+            <div className="Arrow_Nav">
+              <img
+                src="./src/assets/T-Shirt/right-arrow-svgrepo-com.png"
+                width="20px"
+                height="20px"
+              />
+            </div>
           </div>
-          <div className="Category_List">
-            <p>T-Shirt</p>
-            <span> &#8594;</span>
+          <div className="Category_List Fill_Color2">
+            <img
+              src="./src/assets/icons/hoodie-svgrepo-com.png"
+              width="50px"
+              height="50px"
+            />
+            <p>Hoodie</p>
+            <div className="Arrow_Nav">
+              <img
+                src="./src/assets/T-Shirt/right-arrow-svgrepo-com.png"
+                width="20px"
+                height="20px"
+              />
+            </div>
           </div>
-          <div className="Category_List">
+          <div className="Category_List Fill_Color3">
+            <img
+              src="./src/assets/icons/shirt-svgrepo-com.png"
+              width="50px"
+              height="50px"
+            />
             <p>T-Shirt</p>
-            <span> &#8594;</span>
+            <div className="Arrow_Nav">
+              <img
+                src="./src/assets/T-Shirt/right-arrow-svgrepo-com.png"
+                width="20px"
+                height="20px"
+              />
+            </div>
           </div>
-          <div className="Category_List">
-            <p>T-Shirt</p>
-            <span> &#8594;</span>
+          <div className="Category_List Fill_Color4">
+            <img
+              src="./src/assets/icons/sunglasses-svgrepo-com.png"
+              width="50px"
+              height="50px"
+            />
+            <p>SunGlasses</p>
+            <div className="Arrow_Nav">
+              <img
+                src="./src/assets/T-Shirt/right-arrow-svgrepo-com.png"
+                width="20px"
+                height="20px"
+              />
+            </div>
           </div>
-          <div className="Category_List">
-            <p>T-Shirt</p>
-            <span> &#8594;</span>
+          <div className="Category_List Fill_Color5">
+            <img
+              src="./src/assets/icons/watch-clock-time-svgrepo-com.png"
+              width="50px"
+              height="50px"
+            />
+            <p>Watch</p>
+            <div className="Arrow_Nav">
+              <img
+                src="./src/assets/T-Shirt/right-arrow-svgrepo-com.png"
+                width="20px"
+                height="20px"
+              />
+            </div>
           </div>
         </div>
       </div>
