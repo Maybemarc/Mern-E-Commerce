81edcdc599d3cfb9b975291702d3235b7bcdc2c8
Maybemarc <marcspectre777@gmail.com>
2025-05-25 23:15:08 +0530

    Routes handled

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,27 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/authController.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/authController.js";
+import { verifyToken } from "../Middleware/protect.js";
+import User from "../Models/User_Model.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/",registerUser)
-router.post("/login", loginUser)
-router.post("/logout", logoutUser)
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
-export default router
+router.get("/me", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(user)
+  } catch (error) {
+    console.log("Error in Checking: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+export default router;
---
+++ Frontend/e-commerce/src/components/Context/AuthProvider.jsx
@@ -0,0 +1,9 @@
+function AuthProvider({ children }) {
+  return (
+    <div>
+      <h1>Context Api</h1>
+    </div>
+  );
+}
+
+export default AuthProvider;
