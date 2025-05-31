95459e0572fe043cef6fcfda011fcbb38477f0ed
Maybemarc <marcspectre777@gmail.com>
2025-05-31 22:37:23 +0530

    minimal chnage

--- Frontend/e-commerce/src/pages/HomePage.jsx
+++ Frontend/e-commerce/src/pages/HomePage.jsx
@@ -22,28 +22,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-//   const getAllProducts = async function () {
-//     try {
-//       const response = await axios.get("http://localhost:3000/api/lookup", {
-//         withCredenetials: true,
-//       });
-//       console.log(response.data.products);
-//       SetProducts(response.data.products);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     getAllProducts();
-//   }, []);
-
-// <h1>Home page</h1>
-//     {!products ? (
-//       <div>
-//         <h3>No blog</h3>
-//       </div>
-//     ) : (
-//       products.map((product) => <h2 key={product.id}>{product.name}</h2>)
-//     )}
