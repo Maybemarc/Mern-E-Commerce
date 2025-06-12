d9a02f23816edcca849cc5846b7161e221e8d9a0
Maybemarc <marcspectre777@gmail.com>
2025-06-12 16:09:12 +0530

    updates

--- Frontend/e-commerce/src/utils/icons.jsx
+++ Frontend/e-commerce/src/utils/icons.jsx
@@ -1,9 +1,9 @@
 const Icons = [
   "/assets/icons/hoodie-svgrepo-com.png",
+  "/assets/icons/shoe-svgrepo-com.png",
   "/assets/icons/shirt-svgrepo-com.png",
   "/assets/icons/sunglasses-svgrepo-com.png",
   "/assets/icons/watch-clock-time-svgrepo-com.png",
-  "/assets/icons/shoe-svgrepo-com.png",
   ];
 
 
